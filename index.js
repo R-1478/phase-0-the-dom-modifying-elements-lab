87b037c41c2679ea5cfae275b93c4c8bef46a16e
evansthitai <evansnt9619@gmail.com>
2023-10-13 23:35:31 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your code here!
+// const newHeader = document.createElement('h1')
+// const body = document.getElementById("main");
+// const heading = document.createElement('h1')
+// const newHeader = heading.setAttribute("id","victory")
+
+
+
+// heading.textContent = "YOUR-NAME is the champion";
+     
+const body = document.querySelector("body");
+const heading = document.createElement('h1');
+heading.setAttribute("id", "victory");
+heading.textContent = "YOUR-NAME is the champion";
+body.appendChild(heading);
+
+const newHeader = document.querySelector('h1#victory');
+
+
+  
+  
+//   element.append(h1)
+// const newHeaderId = 
+// const newHeaderMessage = newHeader.innerHTML("YOUR-NAME is the champion")
+
